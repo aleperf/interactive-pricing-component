5d522f72a00ca2e8099275b07275f5312b5dac7f
aleperf <aleperf@users.noreply.github.com>
2021-04-06 20:34:22 +0200

    refactor javascript and add checkbox

--- src/js/main.js
+++ src/js/main.js
@@ -7,30 +7,30 @@ const run = document.getElementById("slider-run");
 const views = document.querySelector(".pricing-card__top-views");
 const price = document.querySelector(".pricing-card__top-price--number");
 
-let mousePosition;
+//let mousePosition;
 let thumbPositionX;
 let thumbOffsetX;
-let offset = [0, 0];
-let div;
 let isDown = false;
-let rect = track.getBoundingClientRect();
-let max_offset = rect.right - rect.left;
-var min_offset = 0;
-let step = max_offset / 4;
+
+const rect = track.getBoundingClientRect();
+const maxOffset = rect.right - rect.left;
+const minOffset = 0;
+const step = maxOffset / 4;
 
 let minValue = 10;
 let maxValue = 1000;
-function thumbPressed(e, isMobile) {
-  console.log("thumb is down");
 
+//TIER VIEWS AND PRICES
+
+//---------------------- MOVING THE THUMB SLIDER ------------------------------------------
+
+function thumbPressed(e, isMobile) {
   isDown = true;
   if (isMobile) {
     thumbOffsetX = thumb.offsetLeft - e.changedTouches[0].clientX;
   } else {
     thumbOffsetX = thumb.offsetLeft - e.clientX;
   }
-
-  //console.log("thumbOffsetX: ", thumbOfffsetX);
   thumb.classList.add("pressed");
 }
 
@@ -39,87 +39,9 @@ function thumbFree() {
   thumb.classList.remove("pressed");
 }
 
-thumb.addEventListener(
-  "mousedown",
-  (e) => {
-    thumbPressed(e, false);
-  },
-  false
-);
-/* thumb.addEventListener(
-  "mousedown",
-  function (e) {
-    console.log("mouse is down");
-
-    isDown = true;
-    offset = [thumb.offsetLeft - e.clientX, thumb.offsetTop - e.clientY];
-    thumb.classList.add("pressed");
-  },
-  true
-); */
-
-thumb.addEventListener("touchstart", (e) => {
-  //   isDown = true;
-  //   // offset = [thumb.offsetLeft - e.clientX, thumb.offsetTop - e.clientY];
-  //   thumbOffsetX = thumb.offsetLeft - e.changedTouches[0].clientX;
-  //   thumb.classList.add("pressed");
-  thumbPressed(e, true);
-});
-
-document.addEventListener("mouseup", thumbFree);
-thumb.addEventListener("touchend", thumbFree, false);
-
-/* document.addEventListener("mouseup", function () {
-  isDown = false;
-  thumb.classList.remove("pressed");
-}); */
-
-//touchmove
-
-//vedere qui https://stackoverflow.com/questions/48698441/jquery-ui-draggable-split-screen
-function thumbMove(e) {
-  e.preventDefault();
-  if (isDown) {
-    mousePosition = {
-      x: e.clientX,
-      y: e.clientY,
-    };
-    let currentLeft = mousePosition.x + offset[0];
-    let currentViews = calcCurrentViewsLabel(currentLeft);
-    if (currentLeft <= max_offset && currentLeft >= min_offset) {
-      let currentViews = calcCurrentViewsLabel(currentLeft);
-      if (currentViews === 1000) {
-        views.innerHTML = `1M PageViews`;
-      } else {
-        views.innerHTML = `${currentViews}K PageViews`;
-      }
-      price.innerHTML = `$${calcCurrentPrice(currentViews)}.00`;
-      thumb.style.left = thumb.style.left = mousePosition.x + offset[0] + "px";
-      run.style.width = mousePosition.x + offset[0] + "px";
-    }
-    console.log("offsetLeft:", thumb.offsetLeft);
-  }
-}
-
-document.addEventListener("mousemove", (e) => {
-  //thumbMove(e);
-  moveThumb(e, false);
-});
-
-//STO CAMBIANDO QUESTO PEZZO
-thumb.addEventListener("touchmove", (e) => {
-  moveThumb(e, true);
-});
-/* thumb.addEventListener("touchmove", (e) => {
-  console.log("touchmove ", e.changedTouches[0].clientX);
-  //thumbMove(e);
-  e.preventDefault();
-  if (isDown) {
-    thumbPositionX = e.changedTouches[0].clientX;
-  }
-  let currentLeft = thumbPositionX + thumbOffsetX;
-
-  if (currentLeft <= max_offset && currentLeft >= min_offset) {
+function updateThumbPosition(currentLeft) {
+  console.log("calling this");
+  if (currentLeft <= maxOffset && currentLeft >= minOffset) {
     let currentViews = calcCurrentViewsLabel(currentLeft);
     if (currentViews === 1000) {
       views.innerHTML = `1M PageViews`;
@@ -127,20 +49,22 @@ thumb.addEventListener("touchmove", (e) => {
       views.innerHTML = `${currentViews}K PageViews`;
     }
     price.innerHTML = `$${calcCurrentPrice(currentViews)}.00`;
-    thumb.style.left = thumb.style.left = thumbPositionX + thumbOffsetX + "px";
-    run.style.width = thumbPositionX + thumbOffsetX + "px";
-  } else if (currentLeft > max_offset) {
+    //thumb.style.left = thumb.style.left = thumbPositionX + thumbOffsetX + "px";
+    thumb.style.left = thumb.style.left = currentLeft + "px";
+    //run.style.width = thumbPositionX + thumbOffsetX + "px";
+    run.style.width = currentLeft + "px";
+  } else if (currentLeft > maxOffset) {
     views.innerHTML = `1M PageViews`;
     price.innerHTML = `$${calcCurrentPrice(maxValue)}.00`;
-    thumb.style.left = thumb.style.left = max_offset + "px";
-    run.style.width = max_offset + "px";
-  } else if (currentLeft < min_offset) {
-    views.innerHTML = `${calcCurrentViewsLabel(min_offset)}K PageViews`;
-    price.innerHTML = `$${calcCurrentPrice(min_offset)}.00`;
-    thumb.style.left = thumb.style.left = min_offset + "px";
-    run.style.width = min_offset + "px";
+    thumb.style.left = thumb.style.left = maxOffset + "px";
+    run.style.width = maxOffset + "px";
+  } else if (currentLeft < minOffset) {
+    views.innerHTML = `${calcCurrentViewsLabel(minOffset)}K PageViews`;
+    price.innerHTML = `$${calcCurrentPrice(minOffset)}.00`;
+    thumb.style.left = thumb.style.left = minOffset + "px";
+    run.style.width = minOffset + "px";
   }
-}); */
+}
 
 function moveThumb(e, isMobile) {
   e.preventDefault();
@@ -152,66 +76,41 @@ function moveThumb(e, isMobile) {
     }
   }
   let currentLeft = thumbPositionX + thumbOffsetX;
-
-  if (currentLeft <= max_offset && currentLeft >= min_offset) {
-    let currentViews = calcCurrentViewsLabel(currentLeft);
-    if (currentViews === 1000) {
-      views.innerHTML = `1M PageViews`;
-    } else {
-      views.innerHTML = `${currentViews}K PageViews`;
-    }
-    price.innerHTML = `$${calcCurrentPrice(currentViews)}.00`;
-    thumb.style.left = thumb.style.left = thumbPositionX + thumbOffsetX + "px";
-    run.style.width = thumbPositionX + thumbOffsetX + "px";
-  } else if (currentLeft > max_offset) {
-    views.innerHTML = `1M PageViews`;
-    price.innerHTML = `$${calcCurrentPrice(maxValue)}.00`;
-    thumb.style.left = thumb.style.left = max_offset + "px";
-    run.style.width = max_offset + "px";
-  } else if (currentLeft < min_offset) {
-    views.innerHTML = `${calcCurrentViewsLabel(min_offset)}K PageViews`;
-    price.innerHTML = `$${calcCurrentPrice(min_offset)}.00`;
-    thumb.style.left = thumb.style.left = min_offset + "px";
-    run.style.width = min_offset + "px";
-  }
+  updateThumbPosition(currentLeft);
 }
 
-/* document.addEventListener(
-  "mousemove",
-  function (event) {
-    event.preventDefault();
-    if (isDown) {
-      mousePosition = {
-        x: event.clientX,
-        y: event.clientY,
-      };
-      let currentLeft = mousePosition.x + offset[0];
-      let currentViews = calcCurrentViewsLabel(currentLeft);
-      if (currentLeft <= max_offset && currentLeft >= min_offset) {
-        let currentViews = calcCurrentViewsLabel(currentLeft);
-        if (currentViews === 1000) {
-          views.innerHTML = `1M PageViews`;
-        } else {
-          views.innerHTML = `${currentViews}K PageViews`;
-        }
-        price.innerHTML = `$${calcCurrentPrice(currentViews)}.00`;
+// -------------------------- ADDING SLIDER LISTENERS ------------------------------
 
-        let numberOfViews = calcCurrentViewsLabel(currentLeft);
-        thumb.style.left = thumb.style.left =
-          mousePosition.x + offset[0] + "px";
-        run.style.width = mousePosition.x + offset[0] + "px";
-      }
-      console.log("offsetLeft:", thumb.offsetLeft);
-    }
+//SLIDER LISTENERS ON DESKTOP (MOUSE)
+thumb.addEventListener(
+  "mousedown",
+  (e) => {
+    thumbPressed(e, false);
   },
-  true
-); */
+  false
+);
+
+document.addEventListener("mouseup", thumbFree);
+
+document.addEventListener("mousemove", (e) => {
+  moveThumb(e, false);
+});
+
+//SLIDER LISTENERS ON MOBILE (TOUCHSCREEN)
+
+thumb.addEventListener("touchstart", (e) => {
+  thumbPressed(e, true);
+});
+
+thumb.addEventListener("touchend", thumbFree, false);
+
+thumb.addEventListener("touchmove", (e) => {
+  moveThumb(e, true);
+});
 
 const calcCurrentViewsLabel = (offsetLeft) => {
-  let currentMin = 0;
-  let currentMax = step - 1;
   let currentOffset = offsetLeft;
-  console.log(offsetLeft);
+
   if (offsetLeft < step) {
     minValue = 10;
     maxValue = 49;
@@ -229,7 +128,7 @@ const calcCurrentViewsLabel = (offsetLeft) => {
     currentMax = step * 3 - 1;
   } else {
     currentMin = step * 3;
-    currentMax = max_offset;
+    currentMax = maxOffset;
     currentOffset = offsetLeft - step * 3;
     minValue = 500;
     maxValue = 1000;

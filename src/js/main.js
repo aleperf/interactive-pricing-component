e79e8f585d82155bd794ebcb6730982c9297e3b0
aleperf <aleperf@users.noreply.github.com>
2021-04-07 20:25:34 +0200

    add functionality for mantaining position of thumb on resize

--- src/js/main.js
+++ src/js/main.js
@@ -14,10 +14,10 @@ let thumbPositionX;
 let thumbOffsetX;
 let isDown = false;
 
-const rect = track.getBoundingClientRect();
-const maxOffset = rect.right - rect.left;
+let rect = track.getBoundingClientRect();
+let maxOffset = rect.right - rect.left;
 const minOffset = 0;
-const step = maxOffset / 4;
+let step = maxOffset / 4;
 
 let minValue = 10;
 let maxValue = 1000;
@@ -33,12 +33,48 @@ checkbox.addEventListener("change", (e) => {
   } else {
     isChecked = false;
   }
-  price.innerHTML = `$${calcCurrentPrice(currentViews)}.00`;
 });
 
-//TIER VIEWS AND PRICES
+/**
+ * - 10K pageviews / $8 per month
+- 50K pageviews / $12 per month
+- 100K pageviews / $16 per month
+- 500k pageviews / $24 per month
+- 1M pageviews / $36 per month
+ * 0-49 50-99 100-499 499-1000
+ * 
+ */
 
-//---------------------- MOVING THE THUMB SLIDER ------------------------------------------
+//INTIALIZE VALUES ON ROTATION CHANGE AND REPOSITION THUMB
+
+window.addEventListener("resize", (e) => {
+  const track = document.getElementById("slider-track");
+  rect = track.getBoundingClientRect();
+  maxOffset = rect.right - rect.left;
+  let step = maxOffset / 4;
+  let cLeft = getPositionFromViews(currentViews, maxOffset, step);
+  thumb.style.left = thumb.style.left = cLeft + "px";
+  run.style.width = cLeft + "px";
+});
+
+function getPositionFromViews(views, maxOffset, step) {
+  let min = 0;
+  if (views === 10) {
+    return 0;
+  } else if (views < 50) {
+    return Math.round((step / 40) * views);
+  } else if (views < 100) {
+    return step + (step / 49) * (views - 50);
+  } else if (views < 500) {
+    return 2 * step + (step / 500) * (views - 100);
+  } else if (views < 1000) {
+    return 3 * step + (step / 500) * (views - 500);
+  } else {
+    return maxOffset;
+  }
+}
+
+//---------------------- MOVING THE SLIDER THUMB------------------------------------------
 
 function thumbPressed(e, isMobile) {
   isDown = true;
@@ -64,20 +100,20 @@ function updateThumbPosition(currentLeft) {
       views.innerHTML = `${currentViews}K PageViews`;
     }
     price.innerHTML = `$${calcCurrentPrice(currentViews)}.00`;
-    //thumb.style.left = thumb.style.left = thumbPositionX + thumbOffsetX + "px";
     thumb.style.left = thumb.style.left = currentLeft + "px";
-    //run.style.width = thumbPositionX + thumbOffsetX + "px";
     run.style.width = currentLeft + "px";
   } else if (currentLeft > maxOffset) {
     views.innerHTML = `1M PageViews`;
     price.innerHTML = `$${calcCurrentPrice(maxValue)}.00`;
     thumb.style.left = thumb.style.left = maxOffset + "px";
     run.style.width = maxOffset + "px";
+    currentViews = 1000;
   } else if (currentLeft < minOffset) {
     views.innerHTML = `${calcCurrentViewsLabel(minOffset)}K PageViews`;
     price.innerHTML = `$${calcCurrentPrice(minOffset)}.00`;
     thumb.style.left = thumb.style.left = minOffset + "px";
     run.style.width = minOffset + "px";
+    currentViews = 10;
   }
 }
 
